c57c75bd45404b83e6e3dde8a7d11920e6c3dd77
Charlie-LinYu <34356450+Charlie-LinYu@users.noreply.github.com>
2017-12-21 17:09:12 +0800

    Delete Gen_Stock.h

--- Gen_Stock.h
+++
@@ -1,8 +0,0 @@
-#ifndef Gen_Stock_H_INCLUDED
-#define Gen_Stock_H_INCLUDED
-#include <vector>
-
-//generate the stock prices with normally distributed random numbers
-std::vector<double> Gen_Stock(double seed, int times, double s0, double delta_t, double miu, double sigma);
-
-#endif // Gen_Stock_H_INCLUDED
